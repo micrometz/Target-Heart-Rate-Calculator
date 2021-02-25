34e058d72552b82d48cae1c8abba73415cdb4cab
Weston Metz <micrometz@gmail.com>
2021-02-25 14:12:00 -0700

    Add files via upload

--- HeartRates.h
+++ HeartRates.h
@@ -4,6 +4,24 @@ using namespace std;
 class HeartRates
 {
    public:
-
+       HeartRates ( string fname, string lname, int mm, int dd, int yy );
+       void setFirstName ( string fname);
+       string getFirstName ();
+       void setLastName (string lname);
+       string getLastName ();
+       void setMonthOfBirth (int mm);
+       int getMonthOfBirth ();
+       void setDayOfBirth (int dd);
+       int getDayOfBirth ();
+       void setYearOfBirth(int yy);
+       int getYearOfBirth();
+       int getAge();
+       int getMaximumHeartRate();
+       string getTargetHeartRate();
    private:
+       string firstName;
+       string lastName;
+       int dayOfBirth;
+       int monthOfBirth;
+       int yearOfBirth;
 };
