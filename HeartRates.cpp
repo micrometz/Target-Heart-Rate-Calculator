2ef955c6096bcb2d2228844c0baa7c779460e41c
Weston Metz <micrometz@gmail.com>
2021-02-25 13:57:15 -0700

    Add files via upload
    add the heartRate

--- HeartRates.cpp
+++ HeartRates.cpp
@@ -2,3 +2,97 @@
 #include <string>
 #include <sstream>
 #include "HeartRates.h"
+using namespace std;
+template <typename T>
+
+std::string toString(T val)
+{
+   std::ostringstream ostring;
+   ostring << val;
+   return ostring.str();
+}
+
+HeartRates::HeartRates ( string fname, string lname, int mm, int dd, int yy )
+{
+   setFirstName(fname);
+   setLastName(lname);
+   setMonthOfBirth(mm);
+   setDayOfBirth(dd);
+   setYearOfBirth(yy);
+}
+void HeartRates::setFirstName ( string fname)
+{
+   firstName = fname;
+}
+
+string HeartRates::getFirstName ()
+{
+   return firstName;
+}
+
+void HeartRates::setLastName (string lname)
+{
+   lastName = lname;
+}
+
+string HeartRates::getLastName ()
+{
+   return lastName;
+}
+
+void HeartRates::setMonthOfBirth (int mm)
+{
+   monthOfBirth = mm;
+}
+
+int HeartRates::getMonthOfBirth ()
+{
+   return monthOfBirth;
+}
+
+void HeartRates::setDayOfBirth (int dd)
+{
+   dayOfBirth = dd;
+}
+
+int HeartRates::getDayOfBirth ()
+{
+   return dayOfBirth;
+}
+
+void HeartRates::setYearOfBirth(int yy)
+{
+   yearOfBirth = yy;
+}
+
+int HeartRates::getYearOfBirth()
+{
+   return yearOfBirth;
+}
+
+int HeartRates::getAge()
+{
+   int delta = 0;
+   int month, day, year;
+   cout << "Todays date: ";
+   cin >> month >> day >> year;    // decide if birthday this year passed
+   if (getMonthOfBirth() > month) delta = -1;
+   if ((getMonthOfBirth() == month) && (getDayOfBirth() > day)) delta = -1;
+   return (year - getYearOfBirth() + delta);  
+}
+
+int HeartRates::getMaximumHeartRate()
+{
+   return (220 - getAge());
+}
+
+string HeartRates::getTargetHeartRate()
+{
+   int max;
+   max = getMaximumHeartRate();
+   string lowrate, highrate;
+   lowrate = toString(max * 50 / 100);
+   highrate = toString(max * 85 / 100);
+   return (lowrate + "-" + highrate);
+}
+
